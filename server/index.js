836836db68b2714dde3a0adff7a0d00b0ba37d4e
webtech2345 <86890342+webtech2345@users.noreply.github.com>
2021-07-05 13:25:54 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://dhruv:webtechproject@cluster0.jxyfs.mongodb.net/MemoriesDB?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://dhruv:webtechproject@webproj.jxyfs.mongodb.net/memoriesDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
